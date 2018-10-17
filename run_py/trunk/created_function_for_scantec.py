4470fa62cd1c528494e3e914f75ec8ece027bfaa
jk-active <slipkorn.jk@gmail.com>
2018-10-17 09:31:29 -0300

    Update created_function_for_scantec.py

--- run_py/trunk/created_function_for_scantec.py
+++ run_py/trunk/created_function_for_scantec.py
@@ -94,3 +94,130 @@ plot_ROC('UVEL-250','UVEL-500','UVEL-850','UVEL','10','ACOR')
 plt.subplot(6,2,11)
 plot_ROC('VVEL-250','VVEL-500','VVEL-850','VVEL','10','ACOR')
 
+# numero 14 - > relacionado a 72h
+plt.figure(figsize=(15,15))
+
+plt.subplot(6, 2, 1)
+plot_ROC('VTMP-925','VTMP-850','VTMP-500','VTMP','14','ACOR')
+
+plt.subplot(6, 2, 3)
+plot_ROC('TEMP-250','TEMP-500','TEMP-850','TEMP','14','ACOR')
+
+plt.subplot(6, 2, 5)
+plot_ROC('UMES-500','UMES-850','UMES-925','UMES','14','ACOR')
+
+plt.subplot(6,2,7)
+plot_ROC('ZGEO-250','ZGEO-500','ZGEO-850','ZGEO','14','ACOR')
+
+plt.subplot(6,2,9)
+plot_ROC('UVEL-250','UVEL-500','UVEL-850','UVEL','14','ACOR')
+
+plt.subplot(6,2,11)
+plot_ROC('VVEL-250','VVEL-500','VVEL-850','VVEL','14','ACOR')
+
+# numero 18 - > relacionado a 96h
+plt.figure(figsize=(15,15))
+
+plt.subplot(6, 2, 1)
+plot_ROC('VTMP-925','VTMP-850','VTMP-500','VTMP','18','ACOR')
+
+plt.subplot(6, 2, 3)
+plot_ROC('TEMP-250','TEMP-500','TEMP-850','TEMP','18','ACOR')
+
+plt.subplot(6, 2, 5)
+plot_ROC('UMES-500','UMES-850','UMES-925','UMES','18','ACOR')
+
+plt.subplot(6,2,7)
+plot_ROC('ZGEO-250','ZGEO-500','ZGEO-850','ZGEO','18','ACOR')
+
+plt.subplot(6,2,9)
+plot_ROC('UVEL-250','UVEL-500','UVEL-850','UVEL','18','ACOR')
+
+plt.subplot(6,2,11)
+plot_ROC('VVEL-250','VVEL-500','VVEL-850','VVEL','18','ACOR')
+
+# numero 22 - > relacionado a 120h
+plt.figure(figsize=(15,15))
+
+plt.subplot(6, 2, 1)
+plot_ROC('VTMP-925','VTMP-850','VTMP-500','VTMP','22','ACOR')
+
+plt.subplot(6, 2, 3)
+plot_ROC('TEMP-250','TEMP-500','TEMP-850','TEMP','22','ACOR')
+
+plt.subplot(6, 2, 5)
+plot_ROC('UMES-500','UMES-850','UMES-925','UMES','22','ACOR')
+
+plt.subplot(6,2,7)
+plot_ROC('ZGEO-250','ZGEO-500','ZGEO-850','ZGEO','22','ACOR')
+
+plt.subplot(6,2,9)
+plot_ROC('UVEL-250','UVEL-500','UVEL-850','UVEL','22','ACOR')
+
+plt.subplot(6,2,11)
+plot_ROC('VVEL-250','VVEL-500','VVEL-850','VVEL','22','ACOR')
+
+# numero 26 - > relacionado a 144h
+plt.figure(figsize=(15,15))
+
+plt.subplot(6, 2, 1)
+plot_ROC('VTMP-925','VTMP-850','VTMP-500','VTMP','26','ACOR')
+
+plt.subplot(6, 2, 3)
+plot_ROC('TEMP-250','TEMP-500','TEMP-850','TEMP','26','ACOR')
+
+plt.subplot(6, 2, 5)
+plot_ROC('UMES-500','UMES-850','UMES-925','UMES','26','ACOR')
+
+plt.subplot(6,2,7)
+plot_ROC('ZGEO-250','ZGEO-500','ZGEO-850','ZGEO','26','ACOR')
+
+plt.subplot(6,2,9)
+plot_ROC('UVEL-250','UVEL-500','UVEL-850','UVEL','26','ACOR')
+
+plt.subplot(6,2,11)
+plot_ROC('VVEL-250','VVEL-500','VVEL-850','VVEL','26','ACOR')
+
+# <><><><><><><><<><>
+
+# numero 6 - > relacionado a 24h
+plt.figure(figsize=(15,15))
+
+plt.subplot(6, 2, 1)
+plot_ROC('VTMP-925','VTMP-850','VTMP-500','VTMP','6','RMSEEXP')
+
+plt.subplot(6, 2, 3)
+plot_ROC('TEMP-250','TEMP-500','TEMP-850','TEMP','6','RMSEEXP')
+
+plt.subplot(6, 2, 5)
+plot_ROC('UMES-500','UMES-850','UMES-925','UMES','6','RMSEEXP')
+
+plt.subplot(6,2,7)
+plot_ROC('ZGEO-250','ZGEO-500','ZGEO-850','ZGEO','6','RMSEEXP')
+
+plt.subplot(6,2,9)
+plot_ROC('UVEL-250','UVEL-500','UVEL-850','UVEL','6','RMSEEXP')
+
+plt.subplot(6,2,11)
+plot_ROC('VVEL-250','VVEL-500','VVEL-850','VVEL','6','RMSEEXP')
+
+# numero 10 - > relacionado a 48h
+plt.figure(figsize=(15,15))
+
+plt.subplot(6, 2, 1)
+plot_ROC('VTMP-925','VTMP-850','VTMP-500','VTMP','6','RMSEEXP')
+
+plt.subplot(6, 2, 3)
+plot_ROC('TEMP-250','TEMP-500','TEMP-850','TEMP','6','RMSEEXP')
+
+plt.subplot(6, 2, 5)
+plot_ROC('UMES-500','UMES-850','UMES-925','UMES','6','RMSEEXP')
+
+plt.subplot(6,2,7)
+plot_ROC('ZGEO-250','ZGEO-500','ZGEO-850','ZGEO','6','RMSEEXP')
+
+plt.subplot(6,2,9)
+plot_ROC('UVEL-250','UVEL-500','UVEL-850','UVEL','6','RMSEEXP')
+
+plt.subplot(6,2,11)
+plot_ROC('VVEL-250','VVEL-500','VVEL-850','VVEL','6','RMSEEXP')
