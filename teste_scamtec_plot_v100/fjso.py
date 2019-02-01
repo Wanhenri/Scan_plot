474cd2a87bdc8ca0e9a8628bab7f2ccc39491cbc
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-02-01 10:20:55 -0200

    Update fjso.py
    organizado o código; efetuado a limpeza;

--- teste_scamtec_plot_v100/fjso.py
+++ teste_scamtec_plot_v100/fjso.py
@@ -9,35 +9,18 @@ with open('json/teste.json', encoding='utf-8') as f:
 
 
 def select(teste1,teste3,teste4,teste5):
-  #teste1 = 5
-  #teste3 = 3
   result1 = []
   result2 = []
   result3 = []
   result4 = []
   result5 = []
-  #for i in range(0,teste1): 
-  #  var1 = data['var_scantec'][0]['position'][i]
-  #  result1.append(var1)
-#
-  #for k in range(0,teste1):   
-  #  var2 = data['var_scantec'][1]['previsao'][k]
-  #  result2.append(var2)
-#
-  #for j in range(0,teste3):    
-  #  var3 = data['var_scantec'][2]['estatistica'][j]
-  #  result3.append(var3)
-
+ 
   for a in range(0,teste1): 
     var1 = data['var_scantec'][0]['position'][a]
     result1.append(var1)
 
     var2 = data['var_scantec'][1]['previsao'][a]
     result2.append(var2)
-
-  #for b in range(0,teste1):   
-  #  var2 = data['var_scantec'][1]['previsao'][b]
-  #  result2.append(var2)
 
   for c in range(0,teste3):    
     var3 = data['var_scantec'][2]['estatistica'][c]
@@ -51,19 +34,5 @@ def select(teste1,teste3,teste4,teste5):
     var5 = data['var_scantec'][4]['level'][e]
     result5.append(var5)
 
-  return (result1,result2,result3,result4,result5)
+  return (result1,result2,result3,result4,result5)   
 
-
-#a =select(6,3)
-#print(a)
-#print(a[0][0])
-
-
-
-
-
-#
-#
-##https://developer.rhino3d.com/guides/rhinopython/python-xml-json/
-##https://codingnetworker.com/2015/10/python-dictionaries-json-crash-course/
-#
