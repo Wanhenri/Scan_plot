7c91cd6cadbb7585bd819dbdbffe306e3827d053
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-02-04 14:30:13 -0200

    Update main.py

--- teste_scamtec_plot_v100/main.py
+++ teste_scamtec_plot_v100/main.py
@@ -1,7 +1,5 @@
 import glob
-import csv
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 #function created
@@ -13,9 +11,7 @@ from time_exec import time_statistics
 
 # Checando as versões para acompanhamento de atualizações
 print()
-print("NumPy version: {}". format(np.__version__))
 print("pandas version: {}". format(pd.__version__))
-print("csv version: {}". format(csv.__version__))
 print()
 
 a =select(6,3,8,4)
@@ -28,17 +24,14 @@ plot_ROC_1_var(str(a[3][7]),str(a[3][8]), a[0][0],a[2][0])
 # Tempo de execucao da funcao
 print('##############')
 print('Funcao: plot_ROC_1_var')
-print('Tempo mínimo [s]: ', min(plot_ROC_1_var.times))
-print('Tempo máximo [s]: ', max(plot_ROC_1_var.times))
 print('Média de tempo [s]: ', sum(plot_ROC_1_var.times) / len(plot_ROC_1_var.times))
 print('##############')
 
 
-#title+level | title+level | title+level | title | position | estatistica
 
-#for tt, ll in itertools.product(range )
 plt.figure(figsize=(10,10))
 #<title+level | title+level | title+level | title | position | estatistica>
+#>>plot_ROC('TEMP-250','TEMP-500','TEMP-850','TEMP','6','ACOR')
 plot_ROC(
          str(a[3][1]) +'-'+ str(a[4][0]),
          str(a[3][1]) +'-'+ str(a[4][0]),
@@ -49,11 +42,14 @@ plot_ROC(
 
 print('##############')
 print('Funcao: plot_ROC')
-print('Tempo mínimo [s]: ', min(plot_ROC.times))
-print('Tempo máximo [s]: ', max(plot_ROC.times))
 print('Média de tempo [s]: ', sum(plot_ROC.times) / len(plot_ROC.times))
 print('##############')
 
+
+
+
+
+####################
 
 ## numero 6 - > relacionado a 24h
 #plt.figure(figsize=(15,15))
