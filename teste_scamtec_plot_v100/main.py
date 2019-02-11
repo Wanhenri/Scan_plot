c7dd66f9f96740bdf81f18abef8b5f81028a0ebe
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-02-11 16:39:03 -0200

    Update main.py

--- teste_scamtec_plot_v100/main.py
+++ teste_scamtec_plot_v100/main.py
@@ -14,47 +14,51 @@ print()
 print("pandas version: {}". format(pd.__version__))
 print()
 
-a =select(6,3,8,4)
+# fjson.py
+a =select(6,3,14,4)
 
 
 for b, c in zip(range(6,9,2), range(7,10,2)):
-    for d, e in itertools.product(range(0,6), range(0,3)):
-        plt.figure(figsize=(10,10))
-        plot_ROC_1_var(str(a[3][b]),str(a[3][c]), a[0][d],a[2][e])
-        
-        print('Funcao: plot_ROC_1_var')
-        print('Média de tempo [s]: ', sum(plot_ROC_1_var.times) / len(plot_ROC_1_var.times))
+        for d, f in zip(range(0,6), range(0,6)):
+                for e in range(0,3):
+                        plt.figure(figsize=(10,10))
+                        plot_ROC_1_var(str(a[3][b]),str(a[3][c]), a[0][d],a[2][e],a[1][f])
 
+                        print('Funcao: plot_ROC_1_var')
+                        print('Média de tempo [s]: ', sum(plot_ROC_1_var.times) / len(plot_ROC_1_var.times))
 
 print("250 - 850")
 for b, c in zip(range(0,4), range(0,4)):
-  for e, f in itertools.product(range(0,6), range(0,3)):
-        plt.figure(figsize=(10,10))
-        plot_ROC(
-                 str(a[3][b]) +'-'+ str(a[4][0]),
-                 str(a[3][b]) +'-'+ str(a[4][1]),
-                 str(a[3][b]) +'-'+ str(a[4][2]),
-                 str(a[3][c]), a[0][e],a[2][f]
-                 )
+        for e, g in zip(range(0,6),range(0,6)):
+                for f in range(0,3):
+                        plt.figure(figsize=(10,10))
+                        plot_ROC(
+                                 str(a[3][b]) +'-'+ str(a[4][0]),
+                                 str(a[3][b]) +'-'+ str(a[4][1]),
+                                 str(a[3][b]) +'-'+ str(a[4][2]),
+                                 str(a[3][c]), a[0][e],a[2][f],
+                                 a[1][g]
+                                 )
 
-        print('Funcao: plot_ROC')
-        print('Média de tempo [s]: ', sum(plot_ROC.times) / len(plot_ROC.times))
+                        print('Funcao: plot_ROC')
+                        print('Média de tempo [s]: ', sum(plot_ROC.times) / len(plot_ROC.times))
 
 
 print("500-925")
 for b, c in zip(range(4,6), range(4,6)):
-  for e, f in itertools.product(range(0,6), range(0,3)):
-        plt.figure(figsize=(10,10))
-        plot_ROC(
-                 str(a[3][b]) +'-'+ str(a[4][1]),
-                 str(a[3][b]) +'-'+ str(a[4][2]),
-                 str(a[3][b]) +'-'+ str(a[4][3]),
-                 str(a[3][c]), a[0][e],a[2][f]
-                 )
-
-        print('Funcao: plot_ROC')
-        print('Média de tempo [s]: ', sum(plot_ROC.times) / len(plot_ROC.times))
-
+        for e, g in zip (range(0,6), range(0,6)):
+                  for f in range(0,3):
+                        plt.figure(figsize=(10,10))
+                        plot_ROC(
+                                 str(a[3][b]) +'-'+ str(a[4][1]),
+                                 str(a[3][b]) +'-'+ str(a[4][2]),
+                                 str(a[3][b]) +'-'+ str(a[4][3]),
+                                 str(a[3][c]), a[0][e],a[2][f],
+                                 a[1][g]
+                                 )
+                
+                        print('Funcao: plot_ROC')
+                        print('Média de tempo [s]: ', sum(plot_ROC.times) / len(plot_ROC.times))
 
 
 
