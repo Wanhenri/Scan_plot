43a06cafee079719684416a4ba4ae65727c85f5c
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-02-12 15:39:39 -0200

    Update roc3.py

--- teste_scamtec_plot_v100/roc3.py
+++ teste_scamtec_plot_v100/roc3.py
@@ -1,7 +1,10 @@
 import glob
 import pandas as pd
+import os
 
-#corrigir script para o modelo atual
+#corrigir script para o modelo atual - Nok
+#corrigir diretorio                  - Nok
+#corrigir nome do file de saida      - ok
 
 path ='.'
 allFiles = glob.glob(path + "/*.scam")
@@ -9,6 +12,7 @@ allFiles = glob.glob(path + "/*.scam")
 list_ = []
 
 for file_ in allFiles:
+    name_file=os.path.splitext(file_)[0]
     df = pd.read_csv(file_,delimiter='\s+')
-    df.to_csv('new_file/'+file_ + '.csv', float_format='%g')
+    df.to_csv('new_file/'+name_file + '.csv', float_format='%g')
     
