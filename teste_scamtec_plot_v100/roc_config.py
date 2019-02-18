b731b2a3f223bf6f21738e084693a6db894f0055
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-02-18 16:27:38 -0300

    Create roc_config.py

---
+++ teste_scamtec_plot_v100/roc_config.py
@@ -0,0 +1,20 @@
+import glob
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+def open_file(statistic,time,var_1):
+        path =r'dadosscantec/' # use your path
+        allFiles = glob.glob(path + "/"+str(statistic)+"EXP"+ str(time) + "*.csv")
+        print("OK")
+        
+        return (allFiles)
+
+
+def figure_plot(title,time,statistic):
+        plt.savefig('img/'+ str(title)+str(statistic)+str(time)+'.png')
+        return
