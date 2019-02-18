3bf1b662198a488ad24c1b62b42b962d79697d6a
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-02-18 16:31:22 -0300

    Update roc1.py

--- teste_scamtec_plot_v100/roc1.py
+++ teste_scamtec_plot_v100/roc1.py
@@ -3,23 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from time_exec import time_statistics
 
-def open_file(statistic,time,var_1):
-        path =r'dadosscantec/' # use your path
-        allFiles = glob.glob(path + "/"+str(statistic)+"EXP"+ str(time) + "*.csv")
-        return (allFiles)
+from roc_config import * 
 
 def figure_config(ts,var_1,title,time,statistic,previsao):
-        plt.plot(ts)
-        plt.title(title+'_'+str(statistic)+str(time)+'_Prev_'+str(previsao)+'_horas')
-        plt.xlabel("Tempo")
-        plt.ylabel(str(statistic))
-        plt.legend([var_1], loc=0)
-        plt.tight_layout()
-        plt.plot()
-        return
-
-
-def peace_figure(ts,var_1,title,time,statistic,previsao):
         plt.plot(ts)
         plt.title(title+'_'+str(statistic)+str(time)+'_Prev_'+str(previsao)+'_horas')
         plt.xlabel("Tempo")
@@ -43,6 +29,6 @@ def plot_ROC_1_var(var_1,title,time,statistic,previsao):
         ts = df.loc[0:107,[var_1]]
         print(file_)
         
-    peace_figure(ts,var_1,title,time,statistic,previsao)
+    figure_config(ts,var_1,title,time,statistic,previsao) 
     figure_plot(title,time,statistic)
     return
